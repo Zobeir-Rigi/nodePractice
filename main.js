1823f11a8d3292f29f721126fbd4556bd78d122a
Zobeir <rigi.zobir@yahoo.com>
2023-03-24 16:10:40 +0000

    package.json

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-// import Http protocol 
-// create a server with something to connet with client which is response and request 
+// import Http protocol
+// create a server with something to connet with client which is response and request
 // server has to listen to a port
- const http = require("http");
- const server = http.createServer((response, request)=>{
-    console.log(response);
- });
- server.listen(3000);
+//  const http = require("http");
+const express = require("express");
+const app = express();
+
+//  const server = http.createServer((response, request)=>{
+//     console.log(response);
+//  });
+app.get("/", function (request, response) {
+    response.send("wow")
+});
+
+app.listen(3000,()=>console.log("hey! client"));
