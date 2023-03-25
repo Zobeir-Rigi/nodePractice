1033bfb9d2feb9c4d5efaef45f762086852afb21
Zobeir <rigi.zobir@yahoo.com>
2023-03-25 14:51:55 +0000

    i understand what is next and we can't have more than one response

--- main.js
+++ main.js
@@ -4,12 +4,13 @@
 //  const http = require("http");
 const express = require("express");
 const app = express();
-
-//  const server = http.createServer((response, request)=>{
-//     console.log(response);
-//  });
-app.get("/", function (request, response) {
-    response.send("wow")
+app.use((req, res, next) => {
+  console.log("first middleware");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("second middleware");
+  res.send("<h1>Hello world</h1>");
 });
 
-app.listen(3000,()=>console.log("hey! client"));
+app.listen(3000, () => console.log("hey! client"));
