153aced0b916aa7f883387e2318f12a4ae64713a
Zobeir <rigi.zobir@yahoo.com>
2023-03-26 13:35:41 +0100

    we only can have one response ????

--- main.js
+++ main.js
@@ -4,13 +4,32 @@
 //  const http = require("http");
 const express = require("express");
 const app = express();
-app.use((req, res, next) => {
-  console.log("first middleware");
+// app.use((req, res, next) => {
+//   console.log("first middleware");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("second middleware");
+//   res.send("<h1>Hello world</h1>");
+//   next()
+// });
+
+app.get("/", function (request, response,next) {
+  const myArr = ["apple", "Kiwi", `orange`];
+  let currentDate = new Date();
+
+  response.send(currentDate.toLocaleTimeString());
+  next()
+});
+
+app.get("/", function (request, response,next) {
+  response.send("Another route");
   next();
 });
-app.use((req, res, next) => {
-  console.log("second middleware");
-  res.send("<h1>Hello world</h1>");
+
+app.get("/", function (request, response) {
+  const someData = [1, 2, 3];
+  response.json(someData);
 });
 
 app.listen(3000, () => console.log("hey! client"));
