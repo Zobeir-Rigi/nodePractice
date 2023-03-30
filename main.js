27acc2cf24e919819ac01a3cde0bb037497fd54d
Zobeir <rigi.zobir@yahoo.com>
2023-03-30 23:44:39 +0100

    I could send a form but req.body is undefind

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-
 const express = require("express");
 const app = express();
+
 app.get("/query", (req, res) => {
   let valueOne = req.query.firstValue;
   let valueTwo = req.query.secondValue;
   console.log(typeof valueOne); // Both are strings so we need to convert them to strings
-  res.send(`Hi , let\'s check the queries :  ${Number(valueOne) * Number (valueTwo)}  `);
-});
-app.all("/secret", (req, res, next) => {
-  console.log("Accessing the secret section ...");
-  next(); // pass control to the next handler
-});
-app.get("/myprofile",(req, res)=>{
-  res.send("I will put my Profile page here")
-});
-app.post("/", (req, res) => {
-  res.send("Got a POST request");
+  res.send(
+    `Hi , let\'s check the queries :  ${Number(valueOne) * Number(valueTwo)}  `
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next();
+app.get("/myprofile", (req, res) => {
+  res.send("I will put my Profile page here");
 });
-// app.use((req, res, next) => {
-//   console.log("second middleware");
-//   res.send("<h1>Hello world</h1>");
+
+// app.use("/", (req, res) => {
+//   res.send("Got a POST request");
 // });
+
+app.use("/form", (req, res) => {
+  console.log(req.body);
+  res.redirect("/")
+});
+
+app.use("/", (req, res) => {
+  res.send(`<form action="/form" method="post"><label>Name</label><input type="text" name="myName"></input>
+<button>submit</button></form>`);
+});
 
 app.listen(3000, () => console.log("hey! client"));
