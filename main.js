e4a26a79374a51e518a6def48f233cd56e31759b
Zobeir <rigi.zobir@yahoo.com>
2023-04-04 07:18:09 +0100

    page not found

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const adminRouters = require("./routes/admin");
 const home = require("./routes/home");
 const myprofile = require("./routes/myprofile");
 const query = require("./routes/query");
 // client server extention
 const app = express();
-app.use(bodyParser.urlencoded({ exteded: false })); // custom middleware
-// routes 
+
+// custom middleware
+app.use(bodyParser.urlencoded({ exteded: false }));
+// routes
 app.use(adminRouters);
 app.use(home);
 app.use(myprofile);
 app.use(query);
 // end of routes
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3001, () => console.log("hey! client"));
--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const rootDir = require("../utils/path");
+
+
 router.get("/", (req, res) => {
   //   res.send(`<form action="/form" method="post"><label>Name</label><input type="text" name="myName"></input>
   // <button>submit</button></form>`);
-  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+  res.sendFile(path.join(rootDir, "views", "index.html"));
 });
 
 module.exports = router;
