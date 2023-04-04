9ac6cb1370afa563b50157ca3bb766f4d5dbddaf
Zobeir <rigi.zobir@yahoo.com>
2023-04-04 07:20:19 +0100

    comment

--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,10 @@ const path = require("path");
 
 const rootDir = require("../utils/path");
 
-
 router.get("/", (req, res) => {
   //   res.send(`<form action="/form" method="post"><label>Name</label><input type="text" name="myName"></input>
   // <button>submit</button></form>`);
-  res.sendFile(path.join(rootDir, "views", "index.html"));
+  res.sendFile(path.join(rootDir, "views", "index.html")); // instead of __dirname , ("..")
 });
 
 module.exports = router;
