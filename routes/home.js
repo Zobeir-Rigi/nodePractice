14ac2e378d213ad140a5a34c3d4c1049ed58053a
Zobeir <rigi.zobir@yahoo.com>
2023-04-04 06:43:23 +0100

    __dirName

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 router.get("/", (req, res) => {
-  res.send(`<form action="/form" method="post"><label>Name</label><input type="text" name="myName"></input>
-<button>submit</button></form>`);
+  //   res.send(`<form action="/form" method="post"><label>Name</label><input type="text" name="myName"></input>
+  // <button>submit</button></form>`);
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
 
-
-module.exports =router
+module.exports = router;
